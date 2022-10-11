2426f7b3d4b8b7acf3a297f0e9a4244eed79bd64
Tiko7454 <tiko7454av@gmail.com>
2022-10-11 17:02:55 +0400

    Added facilities and removed inconvenient tools is_in was replaced with is_operator added abstract class Convertor global OPERATORS was moved to Convertor's protected field                  and renamed to _OPERATORS added is_special_symbol to detect operators and parentheses

--- postfix2infix/convertor.cxx
+++ postfix2infix/convertor.cxx
@@ -1,12 +1,20 @@
 #include "convertor.hxx"
 
+const std::array<char, 4> Convertor::_OPERATORS{'+', '-', '*', '/'};
 
-bool is_in(char ch, const std::array<char, 4>& arr) {
-    for (char el : arr) { 
+bool Convertor::is_operator(char ch) {
+    for (char el : _OPERATORS) {
         if (ch == el) {
             return true;
         }
     }   
     return false;
+}
+
+bool Convertor::is_operator(std::string ch) {
+    if (ch.size() != 1) {
+        return false;
+    }
+    return is_operator(ch[0]);
 }
 
--- postfix2infix/convertor.hxx
+++ postfix2infix/convertor.hxx
@@ -1,12 +1,23 @@
-#ifndef CONVERTORS_HXX
-#define CONVERTORS_HXX
+#ifndef CONVERTOR_HXX
+#define CONVERTOR_HXX
 #include <string>
 #include <array>
-#include "postfix_to_infix_convertor.hxx"
 
-std::string infix_to_postfix(const std::string& expr);
-const std::array<char, 4> OPERATORS {'+', '-', '*', '/'};
-
-bool is_in(char ch, const std::array<char, 4>& arr);
+class Convertor {
+public:
+    virtual std::string convert() = 0;
+protected:
+    const std::string _expr;
+    static const std::array<char, 4> _OPERATORS;
+    Convertor(const std::string& expr)
+        : _expr{expr} {}
+public:
+    static bool is_operator(char ch);
+    static bool is_operator(std::string ch);
+    static bool is_special_symbol(char ch) {
+        return ch == '(' or ch == ')' or is_operator(ch);
+    }
+    virtual ~Convertor() = default;
+};
 
 #endif
